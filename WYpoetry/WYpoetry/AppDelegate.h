d5fac9b9188271f1ae3f459f66c6aa0b87fef4b2
Wyman <wyman.lyu@gmail.com>
2016-04-11 16:49:14 +0800

    初始化项目

---
+++ WYpoetry/WYpoetry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYpoetry
+//
+//  Created by sialice on 16/4/11.
+//  Copyright © 2016年 wyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYpoetry/WYpoetry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYpoetry
+//
+//  Created by sialice on 16/4/11.
+//  Copyright © 2016年 wyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
